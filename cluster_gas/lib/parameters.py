Corrected error in alpha calculation.
Was calculated using beta(n, C_1=1), not b_n. Now corrected.

cluster_gas/lib/parameters.py
@@ -37,8 +37,11 @@ def alpha(n):  # Calculate evaporation rate from (n+1) clusters.
     exponent = (36*np.pi*mon_vol**2)**(1./3.) * \
                (((n+1)**(2./3.)*sigma(n+1)    - \
                n**(2./3.)*sigma(n) - sigma(1))) / (k_B*T)
+               
+    kappa = D/(jump_freq*d_jump)
+    R_n = ((3*n*mon_vol)/(4*np.pi))**(1./3.)
 
-    return beta(n, 1.)*C_0*np.exp(exponent)
+    return 4*np.pi*(R_n**2/(R_n + kappa))*(D/mon_vol)*C_0*np.exp(exponent)
 
 def critical_radius(T=T):
     assert(T < 273.15), "System not supercooled: please lower the temperature."