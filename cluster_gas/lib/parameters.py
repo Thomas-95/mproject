0cd8db82c7a047521bf2df132b5d587058b5ddf6
Thomas-95 <thomass4695@gmail.com>
2017-02-09 13:24:24 +0000

    Start writing system as classes.
    This allows multiple results to be generated and then compared easily - need to start populating the results folder...

--- cluster_gas/lib/parameters.py
+++ cluster_gas/lib/parameters.py
@@ -5,43 +5,10 @@ import numpy as np
 # parameters with which the system is set up. 
 # -----------------------------------------------------------------------------
 
-# Define some constants.
-D = 2.32e-9            # Mass diffusion coefficient (298.16K, 1atm).
-mon_vol = 2.992e-29    # Monomer volume, taken as vol of one water molecule.
-k_B = 1.38e-23         # Boltzmann constant. 
-T = 272.85             # Temperature of the system. 
-jump_freq = 1.e13      # Jump freq. taken as vibration freq of water molecule.
-d_jump = 2.5e-10       # Jump distance for monomer to attach to cluster.
-                       # Taken as length of Hydrogen bond.
-#jump_freq = 1.        # COULD THESE BE FITTED EMPIRICALLY?
-#d_jump = 1.
-C_0 = 1.               # All sites are available for homogeneous nucleation.
+# Could jump_freq, d_jump be fitted empirically?
+
 sigma_const = (28.0 + (T-273.15)/4.)*1e-4      # -36 < T < 0 Celcius.
 
-
-def beta(n, C_1):  # n is the class of clusters. 
-
-    R_n = ((3*n*mon_vol)/(4*np.pi))**(1./3.)
-    kappa = D/(jump_freq*d_jump)
-
-    return 4*np.pi*(R_n**2/(R_n + kappa))*(D/mon_vol)*C_1
-
-def sigma(n):          # Surface free energy in generalised capillary approx.
-
-    n_0 = ((32e-30*np.pi)/(3*mon_vol))**(1./3.)    # Tolman, 1949.
-
-    return sigma_const*(1 + (n_0/float(n))**(1./3.))**-2
-
-def alpha(n):  # Calculate evaporation rate from (n+1) clusters.
-
-    exponent = (36*np.pi*mon_vol**2)**(1./3.) * \
-               (((n+1)**(2./3.)*sigma(n+1)    - \
-               n**(2./3.)*sigma(n) - sigma(1))) / (k_B*T)
-               
-    kappa = D/(jump_freq*d_jump)
-    R_n = ((3*n*mon_vol)/(4*np.pi))**(1./3.)
-
-    return 4*np.pi*(R_n**2/(R_n + kappa))*(D/mon_vol)*C_0*np.exp(exponent)
 
 def critical_radius(T=T):
     assert(T < 273.15), "System not supercooled: please lower the temperature."
@@ -54,3 +21,53 @@ def critical_radius(T=T):
     return -(2./3.)*(36*np.pi*mon_vol**2)**(1./3.)*(sigma_const/G_nuc)
 
 # Implement a binary search algorithm to see where alpha = beta to check above.
+
+
+class ClusterSystem():
+
+    k_B = 1.38e-23                  # Boltzmann constant. 
+    
+    def __init__(self, n_class, T, D=2.32e-9, mon_vol=2.992e-29, \
+                       d_jump=2.5e-10, jump_freq=1.e13):
+                       
+        self.n_class = n_class      # Number of cluster sizes to sample.
+        self.T = T                  # Temperature of the system. 
+        self.D = D                  # Mass diffusion coeff't (298.16K, 1atm).
+        self.mon_vol = mon_vol      # Monomer volume = one water molecule vol.
+        self.C_0 = 1.               # All sites poss for homogeneous nucleation.
+        self.d_jump = d_jump        # Jump distance for mon to attach to cluster.
+        self.jump_freq = jump_freq  # Taken as vibration freq of water molecule.
+        
+        
+    @staticmethod
+    def beta(n, C_1):  # n = the class of clusters, C_1 is monomer conc.
+
+        R_n = ((3*n*mon_vol)/(4*np.pi))**(1./3.)
+        kappa = D/(jump_freq*d_jump)
+
+    return 4*np.pi*(R_n**2/(R_n + kappa))*(D/mon_vol)*C_1  
+    
+          
+    @staticmethod      
+    def sigma(n):      # Surface free energy in generalised capillary approx.
+
+        n_0 = ((32e-30*np.pi)/(3*mon_vol))**(1./3.)    # Tolman, 1949.
+
+    return sigma_const*(1 + (n_0/float(n))**(1./3.))**-2
+          
+          
+    @staticmethod
+    def alpha(n):      # Calculate evaporation rate from (n+1) clusters.
+
+        exponent = (36*np.pi*mon_vol**2)**(1./3.) * \
+                   (((n+1)**(2./3.)*sigma(n+1)    - \
+                   n**(2./3.)*sigma(n) - sigma(1))) / (k_B*T)
+               
+        kappa = D/(jump_freq*d_jump)
+        R_n = ((3*n*mon_vol)/(4*np.pi))**(1./3.)
+
+    return 4*np.pi*(R_n**2/(R_n + kappa))*(D/mon_vol)*C_0*np.exp(exponent)
+    
+    
+    
+
--- cluster_gas/main.py
+++ cluster_gas/main.py
@@ -2,10 +2,11 @@ import numpy as np
 from lib import ODE_int, generate_matrix, parameters as pr
 import matplotlib.pyplot as plt
 
-N_ITER = 2500
-n_class = 3
+N_ITER = 5000
+n_class = 10
 C_init = np.zeros(shape=(n_class+1))
-C_init[0] = 1.
+C_init[0] = 1.0
+
 
 def calc_mass(x):
     return sum((index+1)*value for index, value in enumerate(x))
@@ -37,20 +38,28 @@ if __name__ == "__main__":
     plt.ylabel("Total mass of clusters.")
     plt.xlabel("Time (s)")
     plt.plot(times, calc_mass([soln[:,i] for i in range(n_class)]))
-    #plt.show()
+    plt.show()
     
     # Check if system is in steady state:
     
-    n=1
+    n=n_class
     R_n = ((3*n*pr.mon_vol)/(4*np.pi))**(1./3.)
     kappa = pr.D/(pr.jump_freq*pr.d_jump)
-    b_1 = 4*np.pi*(R_n**2/(R_n + kappa))*(pr.D/pr.mon_vol) # From beta() funct.
+    b_N = 4*np.pi*(R_n**2/(R_n + kappa))*(pr.D/pr.mon_vol) # From beta() funct.
     
-    exponent = ((n_class + 1)**(2./3.)*pr.sigma(n_class + 1) - \
+    exponent = (36*np.pi*pr.mon_vol**2)**(1./3.) * \
+               ((n_class)**(2./3.)*pr.sigma(n_class) - \
                (2./3.)*pr.sigma(2.) - n_class*pr.sigma(1.))/(pr.k_B * pr.T)
-               
-    print (n_class + 1)**(2./3.)*pr.sigma(n_class + 1) 
-    print (2./3.)*pr.sigma(2.)  
-    print n_class*pr.sigma(1.)
-    print pr.k_B * pr.T
-    print (b_1/np.exp(exponent)) * soln[-1][0]**n_class
+
+    print (b_N/np.exp(exponent)) * soln[-1][0]**n_class
+    print soln[-1][-2]
+    
+    #print b_1 * soln[-1][0]**2, pr.alpha(2) * soln[-1][1]
+    #print b_1 * soln[-1][0] * soln[-1][1], pr.alpha(3) * soln[-1][2]
+    
+    
+    
+    
+    
+    
+    
