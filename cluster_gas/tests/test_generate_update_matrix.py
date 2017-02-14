8f2fa39a527bbd44638d6789397a386e9cb41444
Thomas-95 <thomass4695@gmail.com>
2017-02-14 17:27:07 +0000

    Test incorrectly fails.
    Now changed to a better test that can see the values of alpha and beta being produced - incearse with the value of the index. 
    
    Despite the two produced matrices being identical, it refuses to work. Will investigate.

--- cluster_gas/tests/test_generate_update_matrix.py
+++ cluster_gas/tests/test_generate_update_matrix.py
@@ -1,20 +1,20 @@
 import pytest, env, numpy as np
 from lib import cluster_system as cs
 
-def beta_test(*args):  return 0.1
-def alpha_test(*args): return 1.0
+def beta_test(n, *args):  return 0.1*n
+def alpha_test(n, *args): return 1.0*n
     
 test_system = cs.ClusterSystem(n_class=5, T=1.)
-test_system.beta = beta_test
+test_system.beta  = beta_test
 test_system.alpha = alpha_test
 
 def test_generate_update_matrix():
 
     x = test_system.generate_update_matrix(C_1=1.)
-    M = np.array([[-0.2,  1.9,  0.9,  0.9,  1. ],
-                  [ 0.1, -1.1,  1. ,  0. ,  0. ],
-                  [ 0. ,  0.1, -1.1,  1. ,  0. ],
-                  [ 0. ,  0. ,  0.1, -1.1,  1. ],
-                  [ 0. ,  0. ,  0. ,  0.1, -1. ]])
-                
+    M = np.array([[-0.2,  3.8,  2.7,  3.6,  5.0 ],
+                  [ 0.1, -2.2,  3. ,  0. ,  0.  ],
+                  [ 0. ,  0.2, -3.3,  4.0,  0.  ],
+                  [ 0. ,  0. ,  0.3, -4.4,  5.  ],
+                  [ 0. ,  0. ,  0. ,  0.4, -5.  ]])
+                  
     assert (x == M).all()
