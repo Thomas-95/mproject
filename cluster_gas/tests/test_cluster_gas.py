a5e7858e19e20e75cb85f0eaa2f5c2a26bb353cb
Thomas-95 <thomass4695@gmail.com>
2017-02-15 16:02:56 +0000

    Created test for steady state condition.
    Part of search for an analytic solution(s) - checks that system reaches steady state by comparing rate equation coefficients.

--- cluster_gas/tests/test_cluster_gas.py
+++ cluster_gas/tests/test_cluster_gas.py
@@ -11,3 +11,22 @@ def test_mass_conservation():
     y = cs.ClusterSystem.calc_mass(test_system.soln[-1])
 
     assert(np.allclose(x, y, rtol=1e-10))
+    
+    
+def test_steady_state():
+
+    test_system = cs.ClusterSystem(n_class=4, T=200.)
+    test_system.solve_system(N_ITER=3000)
+    soln = test_system.soln
+    beta = test_system.beta
+    alpha = test_system.alpha
+    
+    assert(np.allclose(soln[-1][0]*beta(1, C_1=soln[-1][0]),
+                       soln[-1][1]*alpha(2),                
+                       rtol = 1e-5))
+    assert(np.allclose(beta(2, C_1=soln[-1][0])*soln[-1][1]/ alpha(3),
+                       soln[-1][2],                  
+                       rtol = 1e-5))
+    assert(np.allclose(beta(3, C_1=soln[-1][0])*soln[-1][2]/ alpha(4),
+                       soln[-1][3],     
+                       rtol = 1e-5))
