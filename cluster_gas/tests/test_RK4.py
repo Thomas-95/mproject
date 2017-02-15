a7070a5e5edf04a8e5e0726d4dd31ea180a5122a
Thomas-95 <thomass4695@gmail.com>
2017-02-15 10:55:21 +0000

    Tests now passing.
    Was an issue with floating point representation - now changed to integers and is fine.
    
    Also added RK4 test for damped pendulum (albeit with high tolerance).

--- cluster_gas/tests/test_RK4.py
+++ cluster_gas/tests/test_RK4.py
@@ -1,8 +1,8 @@
-import pytest, env
-import numpy as np
+import pytest, env, numpy as np
 from lib import ODE_int
 
-def test_RK4():                      # Example of RK4 for a simple pendulum.
+
+def test_RK4_pendulum():             # Example of RK4 for a simple pendulum.
 
     D_hat = 0.                       # Scaled damping constant.
     theta_0 = 0.1                    # Initial displacement. v_0 = 0. 
@@ -11,3 +11,13 @@ def test_RK4():                      # Example of RK4 for a simple pendulum.
     soln = ODE_int.RK4(L, y, h=0.02, n_iter=2500)
     assert np.allclose(soln[-1][1],  0.1*np.cos(50), rtol=1e-10)
     assert np.allclose(soln[-1][0], -0.1*np.sin(50), rtol=1e-10)
+    
+
+def test_RK4_damped_pendulum():      # Example of RK4 for a damped pendulum.
+
+    D_hat = 0.1
+    theta_0 = 0.1
+    y = np.array([0,theta_0])
+    L = np.array([[-D_hat,-1],[1,0]])
+    soln = ODE_int.RK4(L, y, h=0.02, n_iter=2500)
+    assert(np.allclose(soln[-1][1], 0.1*np.exp(-0.05*50)*np.cos(50), rtol=0.4e-1))
--- cluster_gas/tests/test_generate_update_matrix.py
+++ cluster_gas/tests/test_generate_update_matrix.py
@@ -1,8 +1,8 @@
 import pytest, env, numpy as np
 from lib import cluster_system as cs
 
-def beta_test(n, *args):  return 0.1*n
-def alpha_test(n, *args): return 1.0*n
+def beta_test(n, *args):  return 1*n
+def alpha_test(n, *args): return 10*n
     
 test_system = cs.ClusterSystem(n_class=5, T=1.)
 test_system.beta  = beta_test
@@ -10,11 +10,11 @@ test_system.alpha = alpha_test
 
 def test_generate_update_matrix():
 
-    x = test_system.generate_update_matrix(C_1=1.)
-    M = np.array([[-0.2,  3.8,  2.7,  3.6,  5.0 ],
-                  [ 0.1, -2.2,  3. ,  0. ,  0.  ],
-                  [ 0. ,  0.2, -3.3,  4.0,  0.  ],
-                  [ 0. ,  0. ,  0.3, -4.4,  5.  ],
-                  [ 0. ,  0. ,  0. ,  0.4, -5.  ]])
-                  
+    x = test_system.generate_update_matrix(C_1=1)                 
+    M = np.array([[-2,  38,  27,  36,  50],
+                  [ 1, -22,  30,   0,   0],
+                  [ 0,   2, -33,  40,   0],
+                  [ 0,   0,   3, -44,  50],
+                  [ 0,   0,   0,   4, -50 ]])
+             
     assert (x == M).all()
