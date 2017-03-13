8c2ed2056845dbbc5bdaf76b9f4439dc748fc5a3
Thomas-95 <thomass4695@gmail.com>
2017-03-13 15:26:56 +0000

    Testing to see what crashes laptop...
    May need alternative to `scipy.integrate.odeint` - that stores a huge list in memory, which becomes problematic at 1e9 iterations...

--- cluster_gas/main.py
+++ cluster_gas/main.py
@@ -4,4 +4,33 @@ if __name__ == "__main__":
 
     #from results import log_distributions
     #from results import scaled_system
-    from results import constant_update_matrix
+    #from results import constant_update_matrix
+    
+    A = 6.022e23
+    h = 1e-50 * A
+    n_class = 10
+    nums = range(1, n_class+1)
+    T = 250
+    N_ITER = 2e8
+    
+    
+    import time
+    start_time = time.time()
+    system = cs.ClusterSystem(n_class, T, 5.6e4)
+    soln = system.wrap_solve(h, N_ITER)
+    print "Time (s) for simulation:", time.time() - start_time
+    print "Time (s) at simulation termination:", h * N_ITER
+    import sys
+    print "Memory (GB) taken by solution:", float(sys.getsizeof(soln))/1073741824.
+    
+    import matplotlib.pyplot as plt
+    plt.semilogy(nums, soln[-1]*A)
+    
+    import numpy as np
+    from scipy.stats import linregress as linr  
+    print linr(nums, np.log(soln[-1]*A))
+    
+    #plt.figure(2)
+    #times = np.linspace(0, h*N_ITER, N_ITER)
+    #plt.plot(times, soln[:,-1])
+    plt.show()
